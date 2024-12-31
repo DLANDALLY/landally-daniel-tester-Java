f0938e88ae0bf2340c6bda98631fe48c1506a6fb
DLANDALLY <landally@icloud.com>
2024-12-31 20:15:26 +0100

    ft5 - ajout de method à tester

--- src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -57,5 +58,55 @@ public class ParkingServiceTest {
         parkingService.processExitingVehicle();
         verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
     }
+
+    @Test
+    public void testProcessIncomingVehicle(){
+        // test de l’appel de la méthode processIncomingVehicle() où tout se déroule comme attendu
+        parkingService.processIncomingVehicle();
+        verify(parkingSpotDAO, Mockito.times(1)).getNextAvailableSlot(any(ParkingType.class));
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+        verify(ticketDAO, Mockito.times(1)).saveTicket(any(Ticket.class));
+    }
+
+    @Test
+   public void processExitingVehicleTestUnableUpdate(){
+        //exécution du test dans le cas où la méthode updateTicket() de ticketDAO renvoie false lors de l’appel
+        // de processExitingVehicle()
+        when(parkingSpotDAO.updateParking(any(ParkingSpot.class))).thenReturn(false);
+        parkingService.processExitingVehicle();
+        verify(parkingSpotDAO, Mockito.times(1)).updateParking(any(ParkingSpot.class));
+        verify(ticketDAO, Mockito.times(0)).updateTicket(any(Ticket.class));
+    }
+
+    @Test
+    public void testGetNextParkingNumberIfAvailable(){
+        //test de l’appel de la méthode getNextParkingNumberIfAvailable() avec pour résultat l’obtention d’un spot
+        // dont l’ID est 1 et qui est disponible.
+        when(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).thenReturn(1);
+        ParkingSpot parkingSpot = parkingService.getNextParkingNumberIfAvailable();
+        verify(parkingSpotDAO, Mockito.times(1)).getNextAvailableSlot(any(ParkingType.class));
+        assertNotNull(parkingSpot);
+        assertEquals(1, parkingSpot.getId());
+    }
+
+    @Test
+    public void testGetNextParkingNumberIfAvailableParkingNumberNotFound(){
+        //test de l’appel de la méthode getNextParkingNumberIfAvailable() avec pour résultat aucun spot disponible
+        // (la méthode renvoie null).
+        when(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).thenReturn(0);
+        assertNull(parkingService.getNextParkingNumberIfAvailable());
+    }
+
+    @Test
+    public void testGetNextParkingNumberIfAvailableParkingNumberWrongArgument(){
+        //test de l’appel de la méthode getNextParkingNumberIfAvailable() avec pour résultat aucun spot
+        // (la méthode renvoie null) car l’argument saisi par l’utilisateur concernant le type de véhicule est erroné
+        // (par exemple, l’utilisateur a saisi 3).
+        when(parkingSpotDAO.getNextAvailableSlot(ParkingType.CAR)).thenReturn(-1);
+        assertNull(parkingService.getNextParkingNumberIfAvailable());
+    }
+
+
+
 
 }
